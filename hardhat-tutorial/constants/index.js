cd152f520033234918d2b2ba43d4098ef850bae9
Udeh Shedrack <udehshedrackugo@gmail.com>
2022-04-14 16:31:02 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xcf99D3f37526eB41DA94800AFC7518DFAcb5684A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
